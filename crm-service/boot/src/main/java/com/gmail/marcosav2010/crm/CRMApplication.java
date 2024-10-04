ba3eaebc85eb66d824ee9abd8c6432a76f906320
marcosav <marcosav2010@gmail.com>
2024-10-04 16:57:04 +0200

    feat: add openAPI spec and spring base application

---
+++ crm-service/boot/src/main/java/com/gmail/marcosav2010/crm/CRMApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.marcosav2010.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CRMApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CRMApplication.class, args);
+  }
+}
